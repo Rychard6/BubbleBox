2bd4c0bd7a5a3b02f1cdb352eb1f38e461fcea64
Rychard Yan Alves de Morais <rychard.moraes_a@stf.jus.br>
2024-06-07 18:41:40 -0300

    primeira versão

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
 import Home from "../app/Components/index";
+import { Metadata } from "next";
 
-export default function page() {
+
+export const metadata: Metadata = {
+  title: "Bubble Box Taguatinga",
+  description: "Bubble Box - Rede de Lavanderias Self Service",
+  openGraph: {
+    images: [
+      {
+        url: "/imagens/logo.png",
+        alt: "Bubble Box Logo"
+      }
+    ]
+  },
+  icons: {
+    icon: "/imagens/favicon.ico"
+  },
+  keywords: ["Lavanderia", "Self Service", "Bubble Box"]
+};
+
+
+export default function Page() {
   return (
-    <Home/>
+    <Home />
   );
 }
